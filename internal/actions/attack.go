8eee72b818e33612c1fc1feb972bc3efdd1ea8bd
mpm1900 <almostmacmiller@gmail.com>
2026-09-06 23:41:48 -0500

    action range move

--- internal/actions/attack.go
+++ internal/actions/attack.go
@@ -22,6 +22,31 @@ func (b AttackBehavior) Publish(action *components.Action, world donburi.World, 
 			Position: event.Position,
 		}
 
+		if action.Range != nil {
+			center := components.Center(selected)
+			distance := center.Distance(event.Position)
+			if distance < action.Range[0] {
+				return
+			}
+			if distance > action.Range[1] {
+				direction := center.Sub(event.Position).Normalized()
+				move_point := event.Position.Add(direction.MulScalar(action.Range[1]))
+				move_event := components.ActionEvent{
+					Action:   Move,
+					Source:   selected.Entity(),
+					Position: move_point,
+				}
+				if shift {
+					actor.QueueActionEvent(world, move_event, true)
+					actor.QueueActionEvent(world, action_event, true)
+				} else {
+					actor.SetActionEvent(world, move_event)
+					actor.PushNextActionEvent(action_event)
+				}
+				continue
+			}
+		}
+
 		actor.QueueActionEvent(world, action_event, shift)
 	}
 }
